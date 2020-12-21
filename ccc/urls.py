b6243e50aa993a37ddf945554e33ca6ac8c437d9
Ariana Ghahary <ghahary.ariana@gmail.com>
2020-12-21 14:36:48 -0800

    second commit

--- ccc/urls.py
+++ ccc/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'ccc'
 urlpatterns = [
-
+    path('', views.home, name="home"),
+    path('about.html', views.about, name="about"),
+    path('team.html', views.team, name="team"),
+    path('portfolio.html', views.portfolio, name="portfolio"),
+    path('join.html', views.join, name="join"),
 ]
--- ccc/views.py
+++ ccc/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html', {})
+
+def about(request):
+    return render(request, 'about.html', {})
+
+def team(request):
+    return render(request, 'team.html', {})
+
+def portfolio(request):
+    return render(request, 'portfolio.html', {})
+
+def join(request):
+    return render(request, 'join.html', {})
